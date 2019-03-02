bf3e865984adc304dab8eeeb14d5f87312d117dd
jvoltci <jvoltci@gmail.com>
2019-03-02 22:39:32 +0530

    FLai fixed get link

--- server.js
+++ server.js
@@ -1,44 +1,66 @@
 const express = require('express');
 const app = express();
 const https = require('https');
+const http = require('http');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 const port = process.env.PORT || 5000;
+
 let url = '';
+let contentType = '';
+let extension = '';
+let file = "paradox";
 
 app.get('/', (req, res) => {
 	res.send("<h1><a href='https://flai-herokuapp.com' >Go to FLai</a></h1>")
 })
 
 app.post('/download', (req, res) => {
-	let post = req.body;
-	let contentType = '';
-	let file = "paradox";
-	url = post.user.url;
+	contentType = '';
+	extension = req.body.user.extension;
+	url = req.body.user.url;
 
-	if(post.user.extension === ".mp4") {
+	if(extension === ".mp4") {
 		contentType = 'video/mp4';
 		file = "ergo";
 	}
-	else if(post.user.extension === ".mp3")
+	else if(extension === ".mp3")
 		contentType = 'audio/mp3';
-	else if(post.user.extension === ".zip")
+	else if(extension === ".zip")
+		contentType = 'application/zip';
+	else
 		contentType = 'application/zip';
 
+	return res.redirect('/link');
 
-	if(url && post.user.extension) {
-		const request = https.get(url, function(response) {
+})
+
+app.get('/link', (req, res) => {
+	if(url && extension) {
+		if(url[4] !== 's') {
+			const request = http.get(url, function(response) {
 			res.writeHead(200, {
-				"Content-Disposition": "attachment;filename=" + file + post.user.extension,
+				"Content-Disposition": "attachment;filename=" + file + extension,
 				'Content-Type': contentType
 			});
 			response.pipe(res);
-		});
+		});	
+		}
+		else {
+			const request = https.get(url, function(response) {
+				res.writeHead(200, {
+					"Content-Disposition": "attachment;filename=" + file + extension,
+					'Content-Type': contentType
+				});
+				response.pipe(res);
+			});
+		}
 	}
+
 	else {
-		res.sendFile(__dirname + '/public/index.html');
+		res.send("<h1><a href='https://flai-herokuapp.com' >Go to FLai</a></h1>");
 	}
 })
 
@@ -53,12 +75,12 @@ app.get('/play', (req, res) => {
 		});
 	}
 	else {
-		res.sendFile(__dirname + '/public/index.html');
+		res.send("<h1><a href='https://flai-herokuapp.com' >Go to FLai</a></h1>");
 	}
 })
 
 app.listen(port, () => {
-    console.log("Listening on *:3000")
+    console.log("Listening on *:5000")
 })
 
 module.exports = app;
