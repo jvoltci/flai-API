64d9ce95bc43d901b0ee78cf0d41b3d6eae0721f
jvoltci <jvoltci@gmail.com>
2019-04-18 22:43:27 +0530

    Added Retain

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://flai.ml");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
