72acf04dc7c05d2ffae6c9859f51280c64926183
jvoltci <jvoltci@gmail.com>
2019-04-18 22:40:47 +0530

    Added Retain

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 const express = require('express');
-const fs = require('fs');
-const readLastLines = require('read-last-lines');
 const cors = require('cors');
 const app = express();
 const https = require('https');
 const http = require('http');
 const bodyParser = require('body-parser');
 
+const knex = require('knex');
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 const port = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://flai.ml");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 let url = '';
+let link = '';
 let contentType = '';
 let extension = '';
 let file = "Paradox";
 let password = '';
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  }
+});
 
 app.get('/', (req, res) => {
 	res.send("<h1><a href='https://flai.ml' >Go to flai</a></h1>")
@@ -29,10 +44,17 @@ app.post('/download', (req, res) => {
 		extension = req.body.user.extension;
 		url = req.body.user.url;
 
-		fs.appendFile('link.txt', url + '\n',(err) => {
-		  if (err) throw err;
-		});
-		
+		db('flai').where('url', '=', url)
+		.then(data => {
+			if(data[0]) {
+				link = data[0].link;
+			}
+			else {
+				let xLink = makeid(7);
+				db('flai').insert({link: xLink, url: url});
+			}
+		})
+
 		if(extension === ".mp4") {
 			contentType = 'video/mp4';
 			file = "Ergo";
@@ -49,16 +71,24 @@ app.post('/download', (req, res) => {
 			contentType = 'application/zip';
 			file = "Paradox";
 		}
-		return res.redirect('/link');
+		return res.redirect('/link/' + link);
 	}
 	else
 		return res.redirect('https://flai.ml');
 
 })
 
-app.get('/link', (req, res) => {
+app.get('/link/:id', (req, res) => {
 
-	readNow();
+	let fetchedLink = req.params.id;
+	db('flai').where('link', '=', fetchedLink)
+		.then(data => {
+			if(data[0]) {
+				url = data[0].url;
+			}
+			else
+				url = '';
+		})
 
 	if(url && extension) {
 		if(url[4] !== 's') {
@@ -99,8 +129,6 @@ app.get('/link', (req, res) => {
 
 app.get('/play', (req, res) => {
 
-	readNow();
-
 	if(url) {
 		if(url[4] !== 's') {
 			try {
@@ -134,9 +162,14 @@ app.get('/play', (req, res) => {
 	}
 })
 
-const readNow = () => {
-	readLastLines.read('link.txt', 1)
-		.then((line) => url = line.toString().replace(/(\r\n|\n|\r)/gm, ""));
+const makeid = (length) => {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
 }
 
 app.listen(port, () => {
