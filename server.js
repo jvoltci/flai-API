f84389c6826673b182f8f42227fb4d66e2a66f62
jvoltci <jvoltci@gmail.com>
2019-04-30 09:10:05 +0530

    Bug fixed

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const knex = require('knex');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://flai.ml");
@@ -212,7 +213,7 @@ const setFileName = () => {
 	}
 }
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(port, () => {
     console.log("Listening on *:5000")
 })
 
