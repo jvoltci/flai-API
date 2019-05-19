b1e998a130ff6b91962a8e9ee39eef66f733dabe
jvoltci <jvoltci@gmail.com>
2019-05-19 15:42:14 +0530

    Fixed torrent

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://flai.ml");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+});*/
 
 let magnetURI = ''
 let url = '';
