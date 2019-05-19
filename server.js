8c9077833664c3e89976ad843068b54eebc3dd98
jvoltci <jvoltci@gmail.com>
2019-05-19 17:09:22 +0530

    Fixed Torrent

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 const port = process.env.PORT || 5000;
+process.setMaxListeners(0);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://flai.ml");
@@ -268,7 +269,6 @@ const setFileName = () => {
 }
 
 
-process.setMaxListeners(0);
 // Important stuffs`
 process.on('uncaughtException', (err) => {
     console.log(err);
