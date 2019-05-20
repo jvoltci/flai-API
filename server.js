e5fdfd1be0fa2fb31c6fbe00d1d73ad132831c2c
jvoltci <jvoltci@gmail.com>
2019-05-20 19:51:24 +0530

    MultiStream Test1

--- server.js
+++ server.js
@@ -312,6 +312,78 @@ app.get('/torrent/:file_name', (req, res, next) => {
 
 });
 
+app.get('/torrents/:file_name', (req, res, next) => {
+
+	try {
+		/*let fetchedLink = req.params.id;
+		db('flai').where('link', '=', fetchedLink)
+		.then(data => {
+			if(data[0]) {
+				magnetURI = data[0].magnetURI;
+			}
+			else {
+				return res.redirect('https://flai.ml/#/error');
+			}
+		})*/
+		if(client.get(magnetURI)) {
+			const torrent = client.get(magnetURI);
+			let id = -1;
+			for(i = 0; i < torrent.files.length; i++) {
+				if(torrent.files[i].name == req.params.file_name) {
+					id = i;
+				}
+			}
+			if(id === -1)
+				return res.redirect('https://flai.ml/#/error');
+
+			const streams = [];
+			for(i = 0; i < torrent.files.length; i++) {
+				streams.push(torrent.files[i].createReadStream())
+			}
+			new MultiStream(streams).pipe(res);
+		}
+		else {
+			client.add(magnetURI, torrent => {
+				
+				let id = -1;
+				for(i = 0; i < torrent.files.length; i++) {
+					if(torrent.files[i].name == req.params.file_name) {
+						id = i;
+					}
+				}
+				if(id === -1)
+					return res.redirect('https://flai.ml/#/error');
+
+				db('flai').where('url', '=', url)
+				.then(data => {
+					if(data[0]) {
+						link = data[0].link;
+					}
+					else {
+						link = "torrent/" + req.params.file_name;
+						db('flai').insert({link: link, url: magnetURI, extension: "magnet"}).returning('*')
+							.then(data => console.log(link));
+					}
+				})
+				const streams = [];
+				for(i = 0; i < torrent.files.length; i++) {
+					streams.push(torrent.files[i].createReadStream())
+				}
+				new MultiStream(streams).pipe(res);
+			})
+			.on('error', (err) => {
+				console.log('Z-', err);
+				res.redirect('https://flai.ml/#/error');
+			});
+		}
+	}
+	catch(e) {
+		console.log("Z-Error: ",e);
+		res.redirect('https://flai.ml/#/error');
+	}
+
+});
+
 const makeid = (length) => {
   let text = "";
   let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
