8df8b53b1f5c8aab0fe02e07c7ebfcfb2f1d38a2
jvoltci <jvoltci@gmail.com>
2019-05-22 11:03:14 +0530

    Testing v91

--- server.js
+++ server.js
@@ -354,8 +354,8 @@ app.get('/torrents/:file_name', (req, res, next) => {
 		    setInterval(() => {
 		    	console.log("Interval ->", alpha, beta);
 		    	if(alpha === beta) {
-		    		heatStream[j].destroy()
-		    		notStreamed.push(`${j}- ${torrent.files[j].name}\n`);
+		    		heatStream[j-1].destroy()
+		    		notStreamed.push(`${j-1}- ${torrent.files[j].name}\n`);
 		    		//zip.append(`${torrent.files[j].name}`, { name: `#${torrent.files[j].name}[Not Downloaded].txt` });
 		    		console.log(notStreamed);
 		    		j++;
