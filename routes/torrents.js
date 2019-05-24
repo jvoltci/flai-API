44af3b38370fab79505e744a55825ab8c9ae76dd
jvoltci <jvoltci@gmail.com>
2019-05-24 11:07:37 +0530

    Testing 122

--- routes/torrents.js
+++ routes/torrents.js
@@ -36,7 +36,7 @@ const handleTorrents = (req, res, next, client, Archiver) => {
 			    const zip = Archiver('zip');
 			    zip.pipe(res);
 
-			    let j = 140; 
+			    let j = 0; 
 
 			    let heatStream = [];
 
@@ -121,7 +121,7 @@ const handleTorrents = (req, res, next, client, Archiver) => {
 				    const zip = Archiver('zip');
 				    zip.pipe(res);
 
-				    let j = 140; 
+				    let j = 0; 
 
 				    let heatStream = [];
 
