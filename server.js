f86845624f1d703d69b5040ccb05b36edb8b1eb1
jvoltci <jvoltci@gmail.com>
2019-05-24 22:54:26 +0530

    Testing 122

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const db = knex({
 
 let magnetURI = '';
 let password = '';
-node = 0; //Something msterious here
+isAllow = 0; //Something msterious here
 
 const client = new WebTorrent();
 const app = express();
