53a9a3adaff5adb2bb5db9b949f91f7fa08c0713
jvoltci <jvoltci@gmail.com>
2019-05-24 08:41:22 +0530

    Testing 122

--- routes/torrents.js
+++ routes/torrents.js
@@ -66,7 +66,7 @@ const handleTorrents = (req, res, next, client, Archiver) => {
 			    	if(j < torrent.files.length) {
 			    		heatStream[j] = torrent.files[j].createReadStream(torrent.files[j].name);	
 			    		heatStream[j].on('data', (chunk) => {
-			    			console.log(`${j} ${chunk}`);
+			    			console.log(`${j} ${chunk.length}`);
 			    			beta += chunk.length;
 			    		}).on('end', (err) => {
 			    			if(j <= torrent.files.length) {
