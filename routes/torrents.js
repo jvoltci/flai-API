27b02953d2cea9de9435bf984eb9520eb0b18598
jvoltci <jvoltci@gmail.com>
2019-05-26 08:02:32 +0530

    Testing

--- routes/torrents.js
+++ routes/torrents.js
@@ -38,19 +38,26 @@ const streamHead = (req, res, next, torrent, client) => {
     });
 
     let heatStream = '', notStreamed = '';
-    let j = 0, alpha = -1, beta = 0;
+    let j = 0, alpha = -1, beta = 0, round = 0;
 
     zip.append(`${beta} bytes`, { name: `[Download Buffers].txt` });
 
     interval = setInterval(() => {
     	if(alpha === beta && j <= torrentFilesNumber) {
-    		if(j < torrentFilesNumber) {
-    			console.log(`*(${j}/${torrentFilesNumber}) | ${torrent.files[j].name} | ${(beta/1000000).toFixed(1)} mb`);
-	    		notStreamed += `${torrent.files[j].name}\n`;
-	    		zip.append(`${beta} bytes`, { name: `[Download Buffers].txt` });
+    		if(round === 3) {
+    			round = 0;
+    			if(j < torrentFilesNumber) {
+	    			console.log(`*(${j}/${torrentFilesNumber}) | ${torrent.files[j].name} | ${(beta/1000000).toFixed(1)} mb`);
+		    		notStreamed += `${torrent.files[j].name}\n`;
+		    		zip.append(`${beta} bytes`, { name: `[Download Buffers].txt` });
+	    		}
+	    		j++;
+	    		autoStreamOnEnd();
     		}
-    		j++;
-    		autoStreamOnEnd();
+    		else {
+    			zip.append(`${beta} bytes`, { name: `[Download Buffers].txt` });
+    			round++;
+    		}
     	}
     	else {
     		zip.append(`${beta} bytes`, { name: `[Download Buffers].txt` });
