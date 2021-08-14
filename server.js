353617b090190abe51da13968c6645047717d35a
J.AI <jvoltci@gmail.com>
2021-08-14 07:36:53 +0530

    fix

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://jvoltci.github.io/flai");
+  res.header("Access-Control-Allow-Origin", "https://jvoltci.github.io");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
