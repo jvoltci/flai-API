803a350d8becf3897e0e8568562538f1e5d96e56
JAI <jvoltci@gmail.com>
2025-02-28 23:56:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use((req, res, next) => {
-  const allowedOrigins = ['https://jvoltci.github.io/flai', 'https://flai.ivehement.com'];
+  const allowedOrigins = ['https://jvoltci.github.io', 'https://jvoltci.github.io/flai', 'https://flai.ivehement.com'];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
        res.setHeader('Access-Control-Allow-Origin', origin);
